34e064acd29d8bf74311b56e23f192db141dba8b
Martin Etchebarne <martin@etchebarne.net>
2026-02-15 15:40:11 -0300

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+export default function Input() {
+  return (
+    <div>
+      <span className="text-accent text-nowrap">~ $ </span>
+      <input className="w-full outline-none" autoFocus />
+    </div>
+  );
+}
