48bc52b8cf2f723b90dec055bbd537140542638c
Martin Etchebarne <martin@etchebarne.net>
2026-02-15 16:23:34 -0300

    chore: add input to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
   return (
-    <div className="max-w-3xl mx-auto py-8 wrap-anywhere text-xs">
+    <div className="max-w-3xl mx-auto py-8 wrap-anywhere space-y-4">
       <Header />
       {/* <Content /> */}
-      {/* <Input /> */}
+      <Input />
     </div>
   );
 }
