8313d35429ad287250ba7c8f5268e64946336711
Martin Etchebarne <martin@etchebarne.net>
2026-02-15 16:07:56 -0300

    fix: autofocus input on each keypress

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useRef } from 'react';
+
 export default function Input() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      if (event.ctrlKey || event.metaKey) return;
+      ref.current?.focus();
+    });
+  }, []);
+
   return (
     <div>
       <span className="text-accent text-nowrap">~ $ </span>
-      <input className="w-full outline-none" autoFocus />
+      <input ref={ref} autoFocus className="w-full outline-none" />
     </div>
   );
 }
