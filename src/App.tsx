b8ed0ff618d1fc3fc603d4a475f96d522e0024e8
Martin Etchebarne <martin@etchebarne.net>
2026-02-15 15:17:49 -0300

    feat: add ascii art header
    eventually i'll make the header be part of a future Content component,
    but for the sake of being organized, i'll just leave it as it is now

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Header from './components/Header';
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="max-w-3xl mx-auto py-8 wrap-anywhere text-xs">
+      <Header />
+      {/* <Content /> */}
+      {/* <Input /> */}
+    </div>
+  );
 }
