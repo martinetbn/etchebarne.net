630c6edda7b8f0171fd697a64f22143085388943
Martin Etchebarne <martin@etchebarne.net>
2026-02-15 19:11:29 -0300

    feat: implement command execution
    when submitting the form, we check if the prompt matches exactly any of
    the listed commands, if there's a match, we call the handler

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,19 @@
 import { useEffect, useRef } from 'react';
+import commands from '../lib/commands';
 
 export default function Input() {
   const ref = useRef<HTMLInputElement>(null);
+
+  const onSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!ref.current?.value) return;
+    for (const command in commands) {
+      if (ref.current.value === command) {
+        commands[command].handler();
+      }
+    }
+    ref.current.value = '';
+  };
 
   useEffect(() => {
     document.addEventListener('keydown', (event) => {
@@ -11,9 +23,11 @@ export default function Input() {
   }, []);
 
   return (
-    <div>
-      <span className="text-accent text-nowrap">~ $ </span>
-      <input ref={ref} autoFocus className="w-full outline-none" />
+    <div className="flex flex-nowrap gap-2.5">
+      <span className="text-accent text-nowrap">~ $</span>
+      <form onSubmit={onSubmit} className="w-full">
+        <input ref={ref} autoFocus className="w-full outline-none" />
+      </form>
     </div>
   );
 }
